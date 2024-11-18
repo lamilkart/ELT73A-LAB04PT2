2d1608f90425c8bf7eb49809636716d5ab75e009
lamilkart <arthursilva.2023@alunos.utfpr.edu.br>
2024-11-18 17:12:35 -0300

    TIM2

--- TIM2PT2/Core/Src/stm32f4xx_it.c
+++ TIM2PT2/Core/Src/stm32f4xx_it.c
@@ -208,6 +208,7 @@ void TIM2_IRQHandler(void)
   /* USER CODE END TIM2_IRQn 0 */
   HAL_TIM_IRQHandler(&htim2);
   /* USER CODE BEGIN TIM2_IRQn 1 */
+  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
 
   /* USER CODE END TIM2_IRQn 1 */
 }
